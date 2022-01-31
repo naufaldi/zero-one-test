d26fb03d78eaec6cd4458605ae3f08a068ffacf2
naufaldi <naufaldi.rafif@gmail.com>
2022-01-31 11:28:08 +0700

    feat: add react testing

--- apps/envelope/specs/index.spec.tsx
+++ apps/envelope/specs/index.spec.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import Index from '../pages/index';
+import { RecoilRoot } from 'recoil';
 
 describe('Index', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Index />);
+    const { baseElement } = render(
+      <RecoilRoot>
+        <Index />
+      </RecoilRoot>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
